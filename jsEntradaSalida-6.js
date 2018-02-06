c4c09a59d9463003a973805a56ea436c352f5652
emilianojail <emiliano.jail@gmail.com>
2018-02-06 15:01:10 -0300

    Ejercicios 6 a 10. E/S
    Se completaron los ejercicios 6,7,8,9 y 10 de la practica E/S de datos.

---
+++ jsEntradaSalida-6.js
@@ -0,0 +1,14 @@
+/*Debemos lograr tomar Los numeros por ID ,
+transformarlos a enteros (parseInt) y Sumarlos.
+Mostar el resulto por medio de "ALERT"*/
+function sumar()
+{
+    var numero1;
+    var numero2;
+    var resultado;
+    numero1 = parseInt(document.getElementById('numeroUno').value);
+    numero2 = parseInt(document.getElementById('numeroDos').value);
+    resultado = numero1 + numero2;
+    alert('la suma es ' + resultado);
+}
+
---
+++ jsEntradaSalida-8.js
@@ -0,0 +1,14 @@
+/*Debemos lograr tomar Los numeros por ID ,
+transformarlos a enteros (parseInt),realizar la operación correcta y 
+mostrar el resto entre el dividendo y el divisor.
+ej.: "El resto es 0 ."*/
+function SacarResto()
+{
+    var numero1;
+    var numero2;
+    var resultado;
+    numero1 = parseInt(document.getElementById('numeroDividendo').value);
+    numero2 = parseInt(document.getElementById('numeroDivisor').value);
+    resultado = numero1 % numero2
+    alert('El resto es ' + resultado );
+}
