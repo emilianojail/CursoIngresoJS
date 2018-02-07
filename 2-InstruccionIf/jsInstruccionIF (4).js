12adb129ccd8e83a2fea277d9b595102ef053717
emilianojail <emiliano.jail@gmail.com>
2018-02-06 22:04:41 -0300

    Ejercicios de 1 a 6
    Completos los ejercicios 1,2,3,4,5 y 6.

--- 2-InstruccionIf/jsInstruccionIF (4).js
+++ 2-InstruccionIf/jsInstruccionIF (4).js
@@ -2,6 +2,10 @@ function Mostrar()
 {
 //tomo la edad  
 
-
+    var edad;
+    edad = document.getElementById('edad').value;
+    if(edad > 12 && edad <= 17){
+        alert('es un adolescente'); 
+    }
 
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -1,8 +1,19 @@
 function Mostrar()
 {
-//tomo la edad  
+//tomo la edad
 
-
-
+    var edad;
+    edad = document.getElementById('edad').value;    
+    if(edad < 13){
+        alert('es un niño'); 
+    }
+    else{
+        if(edad < 18){
+            alert('es un adolescente'); 
+        }
+        else{
+            alert('es un adulto'); 
+        }
+    }
 
 }//FIN DE LA FUNCIÓN
