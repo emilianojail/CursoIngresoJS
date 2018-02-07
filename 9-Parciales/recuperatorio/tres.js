941ec202d5a6ef004638f2d78467ffa0204deb67
emilianojail <emiliano.jail@gmail.com>
2018-02-06 21:03:12 -0300

    Ejercicio 3 del parcial 2
    Se completo el ejercicio.

--- 9-Parciales/recuperatorio/tres.js
+++ 9-Parciales/recuperatorio/tres.js
@@ -1,4 +1,21 @@
 function Mostrar()
 {
+    var precio1;
+    var precio2;
+    var precio3;
+    var resultadoDeSuma;
+    var resultadoDePromedio;
 
+    precio1 = document.getElementById('precioUno').value;
+    precio2 = document.getElementById('precioDos').value;
+    precio3 = document.getElementById('precioTres').value;
+
+    precio1 = parseInt(precio1);
+    precio2 = parseInt(precio2);
+    precio3 = parseInt(precio3);
+
+    resultadoDeSuma = precio1 + precio2 + precio3;
+    resultadoDePromedio = resultadoDeSuma / 3;
+
+    alert('El resultado de la suma es ' + resultadoDeSuma + ' y el promedio es ' + resultadoDePromedio);    
 }
